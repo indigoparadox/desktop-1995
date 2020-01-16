833a95ebc309aebb92bd5ca3f6b3b664172a7e37
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-01-15 23:07:02 -0500

    Added toolbar dropdowns and wordpad font support.

--- control.js
+++ control.js
@@ -9,6 +9,7 @@ var settings = $.extend( {
     'classes': [],
     'icon': null,
     'callback': null,
+    'items': [],
     'w': 0,
     'h': 0,
 }, options );
@@ -45,23 +46,73 @@ case 'toolbar':
     if( 'destroy' == action ) {
         this.removeClass( 'window-toolbar' );
         return this.children( '.toolbar' ).remove();
+
     } else if( 'create' == action ) {
         this.addClass( 'window-toolbar' );
         var toolBarHandle = $('<div class="toolbar"></div>');
+        for( var i = 0 ; settings.classes.length > i ; i++ ) {
+            toolBarHandle.addClass( settings.classes[i] );
+        }
         toolBarHandle.insertBefore( this.children( '.window-form' ) );
         return toolBarHandle;
+
     }
     return this;
 
 case 'toolbardivider':
     var divider = $('<div class="toolbar-divider"></div>');
-    this.children( '.toolbar' ).append( divider );
+    if( this.hasClass( 'toolbar' ) ) {
+        this.append( divider );
+    } else {
+        this.children( '.toolbar' ).append( divider );
+    }
     return divider;
 
 case 'toolbarbutton':
     settings.w = 20;
     settings.h = 20;
-    return this.children( '.toolbar' ).control95( 'button', action, settings );
+    if( this.hasClass( 'toolbar' ) ) {
+        return this.control95( 'button', action, settings );
+    } else {
+        return this.children( '.toolbar' ).control95( 'button', action, settings );
+    }
+
+case 'toolbardropdown':
+    if( this.hasClass( 'toolbar' ) ) {
+        return this.control95( 'dropdown', action, settings );
+    } else {
+        return this.children( '.toolbar' ).control95( 'dropdown', action, settings );
+    }
+
+case 'dropdown':
+    if( 'destroy' == action ) {
+
+    } else if( 'create' == action ) {
+        var drop = $('<div class="select-wrapper"><select class="input-select"></select></div>');
+        this.append( drop );
+
+        if( null != settings.id ) {
+            drop.attr( 'id', settings.id );
+        }
+        
+        for( var i = 0 ; settings.classes.length > i ; i++ ) {
+            drop.addClass( settings.classes[i] );
+        }
+
+        for( var i = 0 ; settings.items.length > i ; i++ ) {
+            drop.children( 'select' ).append( '<option value="' + i.toString() + '">' +
+                settings.items[i] + '</option>' );
+        }
+        
+        // Attach callback if there is one.
+        /*if( null != settings.callback ) {
+            drop.click( function( e ) {
+                settings.callback( e );
+                e.preventDefault();
+            }, settings.cbData );
+        }*/
+    }
+    return drop;
 
 case 'button':
     if( 'destroy' == action ) {
